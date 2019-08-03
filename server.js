824762cf1ff48bb6dbd7dcdabd34ae8671b4ee74
superuser-chi <mavusogiftntokozo@gmail.com>
2019-08-03 21:03:41 +0200

    updating package.json

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+let app = express();
+app.use(serveStatic(__dirname + "/dist"));
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
-	res.render('index');
-});
-
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
 });
