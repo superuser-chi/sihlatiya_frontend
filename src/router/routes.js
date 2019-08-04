dd10e99381f5406ea87763120f993e0c0c321495
superuser-chi <mavusogiftntokozo@gmail.com>
2019-08-04 07:41:15 +0200

    update my app

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,12 +7,10 @@ const Dashboard = () => import( /* webpackChunkName: "dashboard" */ "@/pages/Das
 const Cashflow = () => import( /* webpackChunkName: "common" */ "@/pages/Cashflow.vue");
 const Notifications = () => import( /* webpackChunkName: "common" */ "@/pages/Notifications.vue");
 const Recommendations = () => import( /* webpackChunkName: "common" */ "@/pages/Recommendations.vue");
-const Maps = () => import( /* webpackChunkName: "common" */ "@/pages/Maps.vue");
+const MarketAnalysis = () => import( /* webpackChunkName: "common" */ "@/pages/MarketAnalysis.vue");
 const BreakEvenAnalysis = () => import( /* webpackChunkName: "common" */ "@/pages/BreakEvenAnalysis.vue");
 const Purchases = () => import( /* webpackChunkName: "common" */ "@/pages/Purchases.vue");
 const Sales = () => import( /* webpackChunkName: "common" */ "@/pages/Sales.vue");
-const Typography = () => import( /* webpackChunkName: "common" */ "@/pages/Typography.vue");
-const TableList = () => import( /* webpackChunkName: "common" */ "@/pages/TableList.vue");
 
 const routes = [{
     path: "/",
@@ -20,7 +18,7 @@ const routes = [{
     redirect: "/dashboard",
     children: [{
         path: "dashboard",
-        name: "Summary",
+        name: "dashboard",
         component: Dashboard
       },
       {
@@ -29,14 +27,9 @@ const routes = [{
         component: Cashflow
       },
       {
-        path: "notifications",
-        name: "notifications",
-        component: Notifications
-      },
-      {
-        path: "maps",
-        name: "maps",
-        component: Maps
+        path: "marketAnalysis",
+        name: "marketAnalysis",
+        component: MarketAnalysis
       },
       {
         path: "break-Even-Analysis",
@@ -58,11 +51,6 @@ const routes = [{
         path: "recommendations",
         name: "recommendations",
         component: Recommendations
-      },
-      {
-        path: "table-list",
-        name: "table-list",
-        component: TableList
       }
     ]
   },
